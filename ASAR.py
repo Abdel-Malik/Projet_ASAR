eb5b312b48150ce64f14b9dab1516579ae554ede
Romain38220 <piccaromain@gmail.com>
2017-04-04 10:21:03 +0200

    Calcul des coefficients d'auto-regression avec inversion classique

--- ASAR.py
+++ ASAR.py
@@ -1,35 +1,27 @@
-# -*- coding: utf-8 -*-
-
-from math import *
 from numpy import *
-
 # Autoc calcule les coefficients d'autocorellation des données extraites du fichier audio
 # L est la liste des données
 def autoc(L):
     n=len(L)
     # r stocke les coefficients auto regressifs
     r=[]
-    r.append(1)
-    for i in range(n-1):
+    for i in range(n):
+        # Calcul des coefficients d'auto-correlation r(i)
         som=0
         for j in range(n-i):
             som+=L[j]*L[i+j]
         r.append(som)
-    # M est la matrice d'autocorellation
+    # M est la matrice d'auto-correlation liée aux données de L
     M=zeros((n-1,n-1))
-    #initialisation de la diagonale
-    for e in range(n-1):
-        M[e,e]=1
-    
-    for d in range(len(r)-1):
-        for k in range(n-d-2):
-            M[d+k+1,k]=r[d+1]
-            M[k,d+k+1]=r[d+1]
+    for d in range(len(r)):
+        for k in range(n-d-1):
+            M[d+k,k]=r[d]
+            M[k,d+k]=r[d]
+    # M_inverse est l'inverse de M
     M_inverse=linalg.inv(M)
+    # r2 est r privé de sa premiere valeur
     r2=array(r[1:])
-    coeff=M.dot(-r2)
+    # coeff est un vecteur contenant les coefficients d'auto-régression
+    coeff=M_inverse.dot(-r2)
     
     return coeff
-           
-    
-    
